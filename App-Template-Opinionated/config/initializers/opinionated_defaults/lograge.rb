192570767f14ed9b5a9ab7b5ef1b1c29264a8ed5
Mike Rogers <me@mikerogers.io>
2020-10-23 11:05:51 +0100

    Running standardrb --fix

--- App-Template-Opinionated/config/initializers/opinionated_defaults/lograge.rb
+++ App-Template-Opinionated/config/initializers/opinionated_defaults/lograge.rb
@@ -1,24 +1,26 @@
 # By default Rails can generate very verbose logs.
 # I'm a fan of: https://github.com/roidrage/lograge
 # It makes every request a single line in the logs.
-Rails.application.configure do
-  config.lograge.enabled = true
-  config.lograge.custom_options = lambda do |event|
-    # No params? Must be ActionCable.
-    return {} unless event.payload[:params].present?
+if defined?(Lograge)
+  Rails.application.configure do
+    config.lograge.enabled = true
+    config.lograge.custom_options = lambda do |event|
+      # No params? Must be ActionCable.
+      return {} unless event.payload[:params].present?
 
-    exceptions = %w[controller action format authenticity_token]
+      exceptions = %w[controller action format authenticity_token]
 
-    {
-      params: event.payload[:params].except(*exceptions)
-    }
+      {
+        params: event.payload[:params].except(*exceptions)
+      }
+    end
+
+    config.lograge.custom_payload do |controller|
+      {
+        host: controller.request.host,
+        remote_ip: controller.request.remote_ip,
+        api_key: controller.request.headers.env["HTTP_X_APIKEY"]
+      }
+    end
   end
-
-  config.lograge.custom_payload do |controller|
-    {
-      host: controller.request.host,
-      remote_ip: controller.request.remote_ip,
-      api_key: controller.request.headers.env['HTTP_X_APIKEY']
-    }
-  end
-end if defined?(Lograge)
+end
--- App-Template-Opinionated/config/initializers/opinionated_defaults/sidekiq.rb
+++ App-Template-Opinionated/config/initializers/opinionated_defaults/sidekiq.rb
@@ -1,8 +1,8 @@
 # Sidekiq is the de-facto standard choice for running Background tasks
 # You should use it to perform API requests & long running tasks.
-if defined?(Sidekiq) && ENV['REDIS_URL'].present?
-  require 'sidekiq/web'
-  require 'sidekiq/cron/web' if defined?(Sidekiq::Cron)
+if defined?(Sidekiq) && ENV["REDIS_URL"].present?
+  require "sidekiq/web"
+  require "sidekiq/cron/web" if defined?(Sidekiq::Cron)
 
   # https://github.com/mperham/sidekiq/wiki/Monitoring#rails-http-basic-auth-from-routes
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
@@ -12,19 +12,21 @@ if defined?(Sidekiq) && ENV['REDIS_URL'].present?
 
   # https://github.com/ondrejbartas/sidekiq-cron
   # Sidekiq-cron - It's a very sensible approach to performing scheduled tasks.
-  Rails.application.config.after_initialize do
-    schedule_file = Rails.root.join('config', 'schedule.yml')
+  if Sidekiq.server? && defined?(Sidekiq::Cron)
+    Rails.application.config.after_initialize do
+      schedule_file = Rails.root.join("config", "schedule.yml")
 
-    # Use https://crontab.guru to confirm times
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if schedule_file.exist?
-  end if Sidekiq.server? && defined?(Sidekiq::Cron)
+      # Use https://crontab.guru to confirm times
+      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if schedule_file.exist?
+    end
+  end
 
   Sidekiq.configure_client do |_config|
     # Any client specific configuration
   end
 
   Rails.application.config.cache_store = :redis_cache_store, {
-    url: ENV['REDIS_URL'],
+    url: ENV["REDIS_URL"],
     network_timeout: 5
   }
   Rails.application.config.active_job.queue_adapter = :sidekiq
